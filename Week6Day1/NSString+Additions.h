205a02fab1dad89851f9bcac41d0f386b129c65e
Lacey Vu <laceylin2010@gmail.com>
2016-03-07 14:44:03 -0800

    finished first task for homework

---
+++ Week6Day1/NSString+Additions.h
@@ -0,0 +1,17 @@
+//
+//  NSString+Additions.h
+//  Week6Day1
+//
+//  Created by Lacey Vu on 3/7/16.
+//  Copyright © 2016 Lacey Vu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (Additions)
+
+- (NSArray *) arrayOfWords;
+
+
+
+@end
---
+++ Week6Day1/Week6Day1/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Week6Day1
+//
+//  Created by Lacey Vu on 3/7/16.
+//  Copyright © 2016 Lacey Vu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "NSString+Additions.h"
+
+int main(int argc, const char * argv[])
+{
+    @autoreleasepool
+    {
+        NSString *colors = @"My favorite color is blue";
+        NSArray *newArray = [colors arrayOfWords];
+        NSLog(@"%@", newArray);
+
+    }
+    return 0;
+}
